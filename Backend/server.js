5766acddd824242a7249e1442c8acdc8ff2ddebb
kausikDas <kd96668@gmail.com>
2026-08-05 09:45:17 +0530

    add origin

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: "https://myportfolio-psi-eight-36.vercel.app",
+  credentials: true,
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI);
